ad96a6bb3d1dc68a459a5d63d1a0c0d2c2ee17cb
Rohit Kumar <nk7565331@gmail.com>
2025-09-19 11:05:57 +0530

    Updated API base URL and CORS setup

--- server/routes/chatbotRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { symptomChecker } = require('../controllers/chatbotController');
-
-// Chatbot ke liye ek hi route hoga (public access)
-router.post('/symptom-check', symptomChecker);
-
-module.exports = router;
